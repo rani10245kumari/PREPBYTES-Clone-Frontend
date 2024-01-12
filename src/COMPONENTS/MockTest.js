63b99741d4a73d4ad52694aba10253e474c5f9c3
rani10245kumari <ranik10245@gmail.com>
2024-01-12 20:17:31 +0530

    2

--- src/COMPONENTS/MockTest.js
+++ src/COMPONENTS/MockTest.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 function MockTest() {
-    return (
-        <div>
+    const [data, setData] = useState([])
 
+    useEffect(() => {
+        axios.get("https://prepbytes-clone-backend-mehz.onrender.com/").then(response => { setData(response.data) })
+
+    })
+    console.log(data)
+    return (
+        <div className='main-container'>
+            <div className='container2'>
+                {data.filter((item) => item.testCategory === "TopicWise").map(item => {
+                    return (
+                        // <div className='item-container' onClick={() => Navigate(`/product/${item.id}`)}>
+                        <div>
+                            <img src={item.testImg} alt='#' className='image' />
+
+                            <h4>{item.testTitle}</h4>
+
+                            <span>${item.testPrice}</span>
+
+
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
