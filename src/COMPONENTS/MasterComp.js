f9a0d924ef0454839ce535d4a3c23eb177eccebd
rani10245kumari <ranik10245@gmail.com>
2024-01-21 12:31:54 +0530

    8

--- src/COMPONENTS/MasterComp.js
+++ src/COMPONENTS/MasterComp.js
@@ -38,6 +38,117 @@ function MasterComp() {
             <div className='container3'>
                 <h5>Get Guidance From Industry Leading Mentors</h5>
                 <p>Other Mentors You Will Be Interacting With</p>
+                <div className='mentor-main'>
+
+
+                    <div className='mentor-sub'>
+                        <img src='/images/empl-1.webp' alt='#' className='mentr'></img>
+                        <div className='descriptn'>
+                            <h3>Mamta</h3>
+                            <p>Co-Founder,PrepBytes</p>
+                            <img src='/images/branda.webp' alt='#' className='company'></img>
+                        </div>
+                    </div>
+
+                    <div className='mentor-sub'>
+                        <img src='/images/empl-2.webp' alt='#' className='mentr'></img>
+                        <div className='descriptn'>
+                            <h3>Kushdeep</h3>
+                            <p>SDE,ShareChat</p>
+                            <img src='/images/brandb.webp' alt='#' className='company'></img>
+                        </div>
+                    </div>
+
+                    <div className='mentor-sub'>
+                        <img src='/images/empl-3.webp' alt='#' className='mentr'></img>
+                        <div className='descriptn'>
+                            <h3>Aveek</h3>
+                            <p>Mentor,PrepBytes</p>
+                            <img src='/images/brandc.webp' alt='#' className='company'></img>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div className='container4'>
+                <h5>Master Competitive Programming With Us</h5>
+
+
+                <div className='contain4-sub'>
+                    <div className='cont4'>
+                        <div className='C4-1'>
+                            <div>
+                                <h4>Personalised Program</h4>
+                                <p>Master your skills from where you are,
+                                    with India's only personalised program</p>
+                            </div>
+                            <img src='/images/tb-1.webp' alt='#'></img>
+                        </div>
+                        <div className='C4-1'>
+                            <div>
+                                <h4>Quick Doubt Support</h4>
+                                <p>Get your doubts cleared by coding
+                                    experts and have a smooth learning</p>
+                            </div>
+                            <img src='/images/tb-2.webp' alt='#'></img>
+                        </div>
+                        <div className='C4-1'>
+                            <div>
+                                <h4>Personalised Program</h4>
+                                <p>Master your skills from where you are,
+                                    with India's only personalised program</p>
+                            </div>
+                            <img src='/images/tb-3.webp' alt='#'></img>
+                        </div>
+                        <div className='C4-1'>
+                            <div>
+                                <h4>Personalised Program</h4>
+                                <p>Master your skills from where you are,
+                                    with India's only personalised program</p>
+                            </div>
+                            <img src='/images/tb-4.webp' alt='#'></img>
+                        </div>
+                    </div>
+                    <div className='cont4'>
+
+                        <img src='/images/MASTER-BIG.svg' alt='#'></img>
+
+                    </div>
+                    <div className='cont4'>
+                        <div className='C4-2'>
+                            <div>
+                                <h4>Personalised Program</h4>
+                                <p>Master your skills from where you are,
+                                    with India's only personalised program</p>
+                            </div>
+                            <img src='/images/tb-5.webp' alt='#'></img>
+                        </div>
+                        <div className='C4-2'>
+                            <div>
+                                <h4>Personalised Program</h4>
+                                <p>Master your skills from where you are,
+                                    with India's only personalised program</p>
+                            </div>
+                            <img src='/images/tb-6.webp' alt='#'></img>
+                        </div>
+                        <div className='C4-2'>
+                            <div>
+                                <h4>Personalised Program</h4>
+                                <p>Master your skills from where you are,
+                                    with India's only personalised program</p>
+                            </div>
+                            <img src='/images/tb-7.webp' alt='#'></img>
+                        </div>
+                        <div className='C4-1'>
+                            <div>
+                                <h4>Personalised Program</h4>
+                                <p>Master your skills from where you are,
+                                    with India's only personalised program</p>
+                            </div>
+                            <img src='/images/tb-8.webp' alt='#'></img>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
