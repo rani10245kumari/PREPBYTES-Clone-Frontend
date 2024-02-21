7f6096fcc631b1e05d6463fd2b19bee1eee97dd4
rani10245kumari <ranik10245@gmail.com>
2024-02-21 18:45:58 +0530

    68

--- src/COMPONENTS/PROJECT/Html.js
+++ src/COMPONENTS/PROJECT/Html.js
@@ -7,7 +7,10 @@ function Html() {
         <div>
             <Project></Project>
             <div className='html-main'>
-                <img src='/Allimg/html.png' alt='#' className='html-left'></img>
+                <div className='html-left'>
+                    <img src='/Allimg/html.png' alt='#' ></img>
+                    <p>HTML (Hypertext Markup Language) is the standard markup language for creating web pages and web applications. Learning HTML provides a foundation for understanding how web pages are structured and how content is presented on the internet</p>
+                </div>
                 <div className='html-right'>
                     <h3>Topics</h3>
                     <div className='html-right2'>
